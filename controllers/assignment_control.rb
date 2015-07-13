5654f7068b0a1e1c7df9a7c515dfc4e523a8437d
Nick <nschette@yahoo.com>
2015-07-13 13:29:47 -0500

    added some controller

---
+++ controllers/assignment_control.rb
@@ -0,0 +1,26 @@
+# This pulls from the main page, and sends the user to the assignment_main page.
+get '/assignment_main' do
+  erb :'assignments/assignment_main'
+end
+
+# form for user to enter information to add assignment
+get "/assignment_add_form" do
+  erb :"/assignments/assignment_add_form"
+end
+
+# This pulls from the assignment main page, and sends the user to the assignment_add page.
+# This page allows the user to add a new assignment name
+get '/assignment_add' do
+  # new_assignment_id is the Integer returned from the add method
+  @new_assignment_id = Assignment.add_to_database(params)
+  erb :"/success/data_added"
+end
+
+get '/assignment_change' do
+  erb :"/assignments/assignment_change"
+end
+
+get '/assignment_list' do
+  @assignment_list = DATABASE.execute('SELECT * FROM assignments') 
+  erb :"/assignments/assignment_list"
+end
